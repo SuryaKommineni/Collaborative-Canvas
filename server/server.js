a492e9699a2f401406995f99b5c30f50fe95d3a8
Surya Kommineni <ksurya73309@gmail.com>
2025-11-07 13:14:58 +0530

    Fix express wildcard route for Render

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ socket.on("disconnect", () => {
 
 app.use(express.static(path.join(__dirname, "../client")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
